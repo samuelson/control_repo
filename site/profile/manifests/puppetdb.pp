b313484dfa04c3d46f9ee8205555a4ca1cadda01
Josh Samuelson <josh@joshsamuelson.com>
2018-03-24 09:46:10 -0700

    Update puppetdb.pp

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -1,4 +1,6 @@
 class profile::puppetdb {
-  include puppetdb
-  include puppetdb::master::config
+  # Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+  # Configure the Puppet master to use puppetdb
+  class { 'puppetdb::master::config': }
 }
