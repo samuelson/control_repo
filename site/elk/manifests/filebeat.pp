c4d1dbfbda666edad3938dd96091864530f96dd2
Josh Samuelson <josh@joshsamuelson.com>
2018-03-23 22:57:07 -0700

    Update filebeat.pp

--- site/elk/manifests/filebeat.pp
+++ site/elk/manifests/filebeat.pp
@@ -6,9 +6,19 @@
 #
 # @example
 #   include elk::filebeat
-class elk::filebeat {
+class elk::filebeat (
+  logstash_server = '127.0.0.1',
+  logstash_port = '127.0.0.1'
+) {
   package {'filebeat':
     ensure => present,
   }
   include elastic_stack::repo
+  file {'/etc/filebeat/filebeat.yml':
+    ensure => file
+    content => epp('elk/filebeat.yml.epp',{
+      logstash_server => $logstash_server,
+      logstash_port => $logstash_port
+    })
+  }
 }
